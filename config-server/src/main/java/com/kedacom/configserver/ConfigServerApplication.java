e6a705cafeaa43255aa59e772509869ca5a66025
suxiongwei <suxiongwei@kaoshixing.com>
2019-08-04 21:17:00 +0800

    配置仓库

--- config-server/src/main/java/com/kedacom/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/kedacom/configserver/ConfigServerApplication.java
@@ -2,12 +2,13 @@ package com.kedacom.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
-@EnableEurekaClient
 public class ConfigServerApplication {
 	//启动项目后访问：http://localhost:1201/eureka-web/prod/master可查看到远程仓库配置文件信息
 	//参考博客；http://blog.didispace.com/spring-cloud-starter-dalston-3/
