5cd8a3c5ba7ae0b3662ac90304ee7313415a9f2f
suxiongwei <suxiongwei@kaoshixing.com>
2019-08-04 09:56:19 +0800

    配置仓库

--- config-server/src/main/java/com/kedacom/configservergit/ConfigServerApplication.java
+++ config-server/src/main/java/com/kedacom/configservergit/ConfigServerApplication.java
@@ -8,10 +8,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableConfigServer
 @SpringBootApplication
 @EnableEurekaClient
-public class ConfigServerGitApplication {
+public class ConfigServerApplication {
 	//启动项目后访问：http://localhost:1201/eureka-web/prod/master可查看到远程仓库配置文件信息
 	//参考博客；http://blog.didispace.com/spring-cloud-starter-dalston-3/
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerGitApplication.class, args);
+		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 }
--- web-app/src/main/java/com/kedacom/keda/controller/HelloController.java
+++ web-app/src/main/java/com/kedacom/keda/controller/HelloController.java
@@ -13,13 +13,13 @@ import org.springframework.web.bind.annotation.*;
  */
 
 @Component
-@Controller
+@RestController
 public class HelloController {
     @RequestMapping("/test")
     public String index(ModelMap map) {
         // 加入一个属性，用来在模板中读取
         map.addAttribute("host", "http://blog.didispace.com");
         // return模板文件的名称，对应src/main/resources/templates/index.html
-        return "index";
+        return "test";
     }
 }
